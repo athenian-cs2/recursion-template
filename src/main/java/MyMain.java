e75902c39b1d0d85e60cd92a07c1ae7091c0b612
sthompson-athenian <sthompson@athenian.org>
2025-08-22 16:06:41 -0700

    Update Gradle to 7.6, Java to 17, and apply code formatting

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,27 +1,26 @@
 public class MyMain {
-    // This method adds up the values of all digits
-    // in x, recursively
-    public static int addDigits(int x) { 
-        // YOUR CODE HERE
-        return -1;
-    }
+  // This method adds up the values of all digits
+  // in x, recursively
+  public static int addDigits(int x) {
+    // YOUR CODE HERE
+    return -1;
+  }
 
+  // This method checks if a String is a palindrome
+  // (e.g. "racecar", "madam"), recursively
+  public static boolean isPalindrome(String str) {
+    // YOUR CODE HERE
+    return false;
+  }
 
-    // This method checks if a String is a palindrome
-    // (e.g. "racecar", "madam"), recursively
-    public static boolean isPalindrome(String str) { 
-        // YOUR CODE HERE
-        return false;
-    }
+  // This method returns the orignal string reversed;
+  // this method should be written using recursion
+  public static String reverse(String str) {
+    // YOUR CODE HERE
+    return "";
+  }
 
-    // This method returns the orignal string reversed;
-    // this method should be written using recursion
-    public static String reverse(String str) { 
-        // YOUR CODE HERE
-        return "";
-    }
-
-    public static void main(String[] args) {
-        // You can test your code here
-    }
+  public static void main(String[] args) {
+    // You can test your code here
+  }
 }
--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,33 +1,66 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class MyTests {
 
-    @Test
-    public void testAddDigits() {
-        assertEquals(4, MyMain.addDigits(1111), "The sum of the digits in 1111 should be: 4");
-        assertEquals(10, MyMain.addDigits(1234), "The sum of the digits in 1234 should be: 10");
-        assertEquals(18, MyMain.addDigits(9342), "The sum of the digits in 9342 should be: 18");
-        assertEquals(21, MyMain.addDigits(1965), "The sum of the digits in 1965 should be: 21");
-    }
+  @Test
+  public void testAddDigits() {
+    assertEquals(4, MyMain.addDigits(1111), "The sum of the digits in 1111 should be: 4");
+    assertEquals(10, MyMain.addDigits(1234), "The sum of the digits in 1234 should be: 10");
+    assertEquals(18, MyMain.addDigits(9342), "The sum of the digits in 9342 should be: 18");
+    assertEquals(21, MyMain.addDigits(1965), "The sum of the digits in 1965 should be: 21");
+  }
 
-    @Test
-    public void testIsPalindrome() {
-        assertEquals(true, MyMain.isPalindrome("racecar"), "Whether or not the String \"racecar\" is a palindrome should be: true");
-        assertEquals(true, MyMain.isPalindrome("naan"), "Whether or not the String \"naan\" is a palindrome should be: true");
-        assertEquals(true, MyMain.isPalindrome("step on no pets"), "Whether or not the String \"step on no pets\" is a palindrome should be: true");
-        assertEquals(true, MyMain.isPalindrome("amanaplanacanalpanama"), "Whether or not the String \"amanaplanacanalpanama\" is a palindrome should be: true");
-        assertEquals(false, MyMain.isPalindrome("lemon"), "Whether or not the String \"lemon\" is a palindrome should be: false");
-        assertEquals(false, MyMain.isPalindrome("abca"), "Whether or not the String \"abca\" is a palindrome should be: false");
-        assertEquals(false, MyMain.isPalindrome("abbcdba"), "Whether or not the String \"abbcdba\" is a palindrome should be: false");
-    }
+  @Test
+  public void testIsPalindrome() {
+    assertEquals(
+        true,
+        MyMain.isPalindrome("racecar"),
+        "Whether or not the String \"racecar\" is a palindrome should be: true");
+    assertEquals(
+        true,
+        MyMain.isPalindrome("naan"),
+        "Whether or not the String \"naan\" is a palindrome should be: true");
+    assertEquals(
+        true,
+        MyMain.isPalindrome("step on no pets"),
+        "Whether or not the String \"step on no pets\" is a palindrome should be: true");
+    assertEquals(
+        true,
+        MyMain.isPalindrome("amanaplanacanalpanama"),
+        "Whether or not the String \"amanaplanacanalpanama\" is a palindrome should be: true");
+    assertEquals(
+        false,
+        MyMain.isPalindrome("lemon"),
+        "Whether or not the String \"lemon\" is a palindrome should be: false");
+    assertEquals(
+        false,
+        MyMain.isPalindrome("abca"),
+        "Whether or not the String \"abca\" is a palindrome should be: false");
+    assertEquals(
+        false,
+        MyMain.isPalindrome("abbcdba"),
+        "Whether or not the String \"abbcdba\" is a palindrome should be: false");
+  }
 
-    @Test
-    public void testReverse() {
-        assertEquals("rohcna", MyMain.reverse("anchor"), "The reverse of the String \"anchor\" should be: rohcna");
-        assertEquals("desserts", MyMain.reverse("stressed"), "The reverse of the String \"stressed\" should be: desserts");
-        assertEquals("abcdefgh", MyMain.reverse("hgfedcba"), "The reverse of the String \"hgfedcba\" should be: abcdefgh");
-        assertEquals("racecar", MyMain.reverse("racecar"), "The reverse of the String \"anchor\" should be: racecar");
-    }
+  @Test
+  public void testReverse() {
+    assertEquals(
+        "rohcna",
+        MyMain.reverse("anchor"),
+        "The reverse of the String \"anchor\" should be: rohcna");
+    assertEquals(
+        "desserts",
+        MyMain.reverse("stressed"),
+        "The reverse of the String \"stressed\" should be: desserts");
+    assertEquals(
+        "abcdefgh",
+        MyMain.reverse("hgfedcba"),
+        "The reverse of the String \"hgfedcba\" should be: abcdefgh");
+    assertEquals(
+        "racecar",
+        MyMain.reverse("racecar"),
+        "The reverse of the String \"anchor\" should be: racecar");
+  }
 }
